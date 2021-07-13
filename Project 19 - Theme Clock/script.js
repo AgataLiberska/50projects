c846ca5e662e5e33c79e347ce47406a93d7f47d7
Agata Liberska <liberska@gmail.com>
2021-07-13 12:27:36 +0100

    add time display

--- Project 19 - Theme Clock/script.js
+++ Project 19 - Theme Clock/script.js
@@ -27,10 +27,13 @@ function setTime() {
     const hoursForClock = hours % 12;
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
 
     hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+
+    timeEl.innerHTML = `${hoursForClock !== 0 ? hoursForClock : `12`}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
